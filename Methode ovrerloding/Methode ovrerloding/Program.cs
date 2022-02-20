5482286d0d09467023fb7aea99c1509494c866ed
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-21 00:58:57 +0530

    Add files via upload

---
+++ Methode ovrerloding/Methode ovrerloding/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static int plusint(int x,int y)
+        {
+            return x + y;
+        }
+        static double plusint(double x,double y)
+        {
+            return x + y;
+        }
+        static void Main(string[]qrg)
+        {
+            int num1=plusint(3,4);
+            double num2=plusint(5.56,6.48);
+            Console.WriteLine("int "+num1);
+            Console.WriteLine("double " + num2);
+
+
+
+        }
+
+        
+
+    }
+}
